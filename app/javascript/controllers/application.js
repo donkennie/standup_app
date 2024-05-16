5cc81ff4158aac960819302eb8c8de431f553ee3
donkennie <ajeigbekehinde160@gmail.com>
2024-05-16 19:23:59 +0100

    rectify the dashboard issue

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,5 @@
 import { Application } from "@hotwired/stimulus"
+import Dropdown from 'stimulus-dropdown'
 
 const application = Application.start()
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
+application.register('dropdown', Dropdown)
